b3522145970c69bffe1de902e274f913e9f92715
Ahmad <ahmadnaderx1998@gmail.com>
2021-01-11 17:46:03 +0200

    completed mock data for homepage

--- Front-End/tazkarty/components/MatchCard.js
+++ Front-End/tazkarty/components/MatchCard.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import styles from "../styles/Card.module.css";
 
-export default function MatchCard() {
+export default function MatchCard(props) {
+console.log("props is ")
+console.log(props)
+var myData = props.data;
 return(
-<div className="card w-25 mx-4"  >
+<div className="card w-25  mx-auto"  >
   <Image className="card-img-top" src="/vs.jpg" alt="Card image cap" width="100%" height="35%" layout="responsive"/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+  <div className="card-body mx-auto justify-content-center">
+    <h5 className="card-title  text-center">{myData.HomeTeam + " vs "+ myData.AwayTeam} </h5>
+    <p className="card-text text-center">{myData.MatchVenue}</p>
+    <p className="card-text text-center">{myData.Date}</p>
+    <p className="card-text text-center">{myData.Time}</p>
+    <p className="card-text text-center">{myData.Referee}</p>
+    <p className="card-text text-center">{myData.Linesman1}</p>
+    <p className="card-text text-center">{myData.Linesman2}</p>
+    <button href="#" className="btn btn-primary text-center ">Go somewhere</button>
   </div>
 </div>
 )}
--- Front-End/tazkarty/pages/index.js
+++ Front-End/tazkarty/pages/index.js
@@ -2,24 +2,42 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import NavBar from "../components/NavBar";
-import MatchCard from "../components/MatchCard"
+import MatchCard from "../components/MatchCard";
 // import logo from '../public/logo.png'; // with import
-export default function Home() {
-  return (<>
-        <NavBar></NavBar>
-        <div>
-        <Image className = "img-fluid" src="/StadiumHD.jpg" alt="" width="100%" height="35%" layout="responsive" />
+export default function Home({ data }) {
+  var matchInfo = { data };
+  console.log("This is matchInfo")
+  console.log(matchInfo.data.matches);
+   var matchCards = matchInfo.data.matches.map(match => <MatchCard data = {match}></MatchCard>)
+  
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>
+        <Image
+          className="img-fluid"
+          src="/StadiumHD.jpg"
+          alt=""
+          width="100%"
+          height="35%"
+          layout="responsive"
+        />
       </div>
-      <div className = "d-flex justify-content-around p-4 flex-wrap">
-      <MatchCard></MatchCard>
-      <MatchCard></MatchCard>
-      <MatchCard></MatchCard>
-      <MatchCard></MatchCard>
-      <MatchCard></MatchCard>
-      <MatchCard></MatchCard>
-      
-
+      <div className="d-flex justify-content-around p-4 flex-wrap">
+        {matchCards}
       </div>
-      </>
+    </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `https://run.mocky.io/v3/31372ede-0d28-46fa-b5ec-854f3aa56b12`
+  );
+  const data = await res.json();
+   console.log(data)
+  var ht = data.HomeTeam;
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
